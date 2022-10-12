234793063a0147b511dfd794a08ca4a9cb7a3fca
Faith-lyle <67628007+Faith-lyle@users.noreply.github.com>
2022-10-12 22:12:20 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -6,15 +6,25 @@
 # @Project : web_demo
 # @Time : 2022/10/11 08:26
 # -------------------------------
+import random
 
 
-def determine_pwd(username,pwd):
+def determine_pwd(username, pwd):
     if username == "admin" and pwd == 'admin':
         return True
     else:
         return False
 
+
+def get_cap_summary():
+    data = {"yield": [], 'qty': []}
+    for i in range(7):
+        data["yield"].append(random.uniform(90.0,96.8))
+        data["qty"].append(random.randint(a=1, b=20))
+    return data
+
+
 if __name__ == '__main__':
-    c = [25,92,180]
+    c = [25, 92, 180]
     for i in c:
         print(hex(i))
