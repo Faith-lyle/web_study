6c91452bb01eb124cae4d682a1ed41189fc5e36c
Faith-lyle <67628007+Faith-lyle@users.noreply.github.com>
2022-10-12 22:14:05 +0800

    Add files via upload

--- static/js/cap.js
+++ static/js/cap.js
@@ -1,3 +1,90 @@
+var yield_chart = echarts.init(document.getElementById('c2'));
+yield_option = {
+  series: [
+    {
+      type: 'gauge',
+      startAngle: 180,
+      endAngle: 0,
+      center: ['50%', '75%'],
+      radius: '90%',
+      min: 0,
+      max: 1,
+      splitNumber: 8,
+      axisLine: {
+        lineStyle: {
+          width: 6,
+          color: [
+            [0.25, '#FF6E76'],
+            [0.5, '#FDDD60'],
+            [0.75, '#58D9F9'],
+            [1, '#7CFFB2']
+          ]
+        }
+      },
+      pointer: {
+        icon: 'path://M12.8,0.7l12,40.1H0.7L12.8,0.7z',
+        length: '12%',
+        width: 20,
+        offsetCenter: [0, '-60%'],
+        itemStyle: {
+          color: 'auto'
+        }
+      },
+      axisTick: {
+        length: 12,
+        lineStyle: {
+          color: 'auto',
+          width: 2
+        }
+      },
+      splitLine: {
+        length: 20,
+        lineStyle: {
+          color: 'auto',
+          width: 5
+        }
+      },
+      axisLabel: {
+        color: 'red',
+        fontSize: 20,
+        distance: -100,
+        rotate: 'tangential',
+        formatter: function (value) {
+          if (value === 0.875) {
+            return 'Perfect';
+          } else if (value === 0.625) {
+            return 'Grade B';
+          } else if (value === 0.375) {
+            return 'Grade C';
+          } else if (value === 0.125) {
+            return 'Grade D';
+          }
+          return '';
+        }
+      },
+      title: {
+        offsetCenter: [0, '-10%'],
+        fontSize: 20
+      },
+      detail: {
+        fontSize: 30,
+        offsetCenter: [0, '-35%'],
+        valueAnimation: true,
+        formatter: function (value) {
+          return Math.round(value * 100)+"%";
+        },
+        color: 'auto'
+      },
+      data: [
+        {
+          value: 0.7,
+          name: 'Yield'
+        }
+      ]
+    }
+  ]
+};
+
 function setCookie(cname,cvalue,exs){
     var d = new Date();
     d.setTime(d.getTime()+(exs*60*1000));
@@ -59,7 +146,7 @@ function on_click(){
 }
 
 
-
+yield_chart.setOption(yield_option);
 get_data()
 update_time()
 setInterval(update_time,1000)
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,275 @@
+var hsgChart = echarts.init(document.getElementById('housing_summary'));
+var capChart = echarts.init(document.getElementById('cap_summary'));
+var idoChart = echarts.init(document.getElementById('ido_summary'));
+hsg_option = {
+  title:{
+    text:"HSG 良率趋势图",
+    left:"center",
+    top:"top",
+    textStyle: {
+      fontSize: 30,
+      color: "white"
+    }
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      crossStyle: {
+        color: '#999'
+      }
+    }
+  },
+  legend: {
+    data: ['Fail QTY', 'Yield'],
+    bottom: "0%",
+    left: "34%",
+    textStyle: {
+      color: "rgba(255, 255, 255, 1)"
+    }
+  },
+  xAxis: [
+    {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      axisPointer: {
+        type: 'shadow'
+      }
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value',
+      name: 'QTY',
+      min: 0,
+      max: 30,
+      interval: 10,
+      axisLabel: {
+        formatter: '{value}'
+      }
+    },
+    {
+      type: 'value',
+      name: 'Yield',
+      min: 85,
+      max: 100,
+      interval: 5,
+      axisLabel: {
+        formatter: '{value} %'
+      }
+    }
+  ],
+  series: [
+    {
+      name: 'Fail QTY',
+      type: 'bar',
+      tooltip: {
+        valueFormatter: function (value) {
+          return value;
+        }
+      },
+      itemStyle: {
+      color: "rgba(255, 17, 0, 1)"
+      },
+      data: [
+        2, 5, 9, 26, 7, 17, 15.6, 12, 8, 8, 6, 3
+      ]
+    },
+    {
+      name: 'Yield',
+      type: 'line',
+      yAxisIndex: 1,
+      lineStyle: {
+      color: "rgba(94, 255, 0, 1)"
+      },
+      tooltip: {
+        valueFormatter: function (value) {
+          return value + ' %';
+        }
+      },
+      data: [92.7, 92.2, 93.3, 94.5, 96.3, 90.2, 92.3, 93.4, 93.0, 96.5, 92.0, 96.2]
+    }
+  ]
+};
+cap_option = {
+  title:{
+    text:"CAP 良率趋势图",
+    left:"center",
+    top:"top",
+    textStyle: {
+      fontSize: 30,
+      color: "white"
+    }
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      crossStyle: {
+        color: '#999'
+      }
+    }
+  },
+  legend: {
+    data: ['Fail QTY', 'Yield'],
+    bottom: "0%",
+    left: "34%",
+    textStyle: {
+      color: "rgba(255, 255, 255, 1)"
+    }
+  },
+  xAxis: [
+    {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      axisPointer: {
+        type: 'shadow'
+      }
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value',
+      name: 'QTY',
+      min: 0,
+      max: 30,
+      interval: 10,
+      axisLabel: {
+        formatter: '{value}'
+      }
+    },
+    {
+      type: 'value',
+      name: 'Yield',
+      min: 85,
+      max: 100,
+      interval: 5,
+      axisLabel: {
+        formatter: '{value} %'
+      }
+    }
+  ],
+  series: [
+    {
+      name: 'Fail QTY',
+      type: 'bar',
+      tooltip: {
+        valueFormatter: function (value) {
+          return value;
+        }
+      },
+      itemStyle: {
+      color: "rgba(255, 17, 0, 1)"
+      },
+      data: [
+        2, 5, 9, 26, 7, 17, 15, 12]
+    },
+    {
+      name: 'Yield',
+      type: 'line',
+      yAxisIndex: 1,
+      lineStyle: {
+      color: "rgba(94, 255, 0, 1)"
+      },
+      tooltip: {
+        valueFormatter: function (value) {
+          return value + ' %';
+        }
+      },
+      data: [92.7, 92.2, 93.3, 94.5, 96.3, 90.2, 92.3]
+    }
+  ]
+};
+ido_option = {
+  title:{
+    text:"IDO 良率趋势图",
+    left:"center",
+    top:"top",
+    textStyle: {
+      fontSize: 30,
+      color: "white"
+    }
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      crossStyle: {
+        color: '#999'
+      }
+    }
+  },
+  legend: {
+    data: ['Fail QTY', 'Yield'],
+    bottom: "0%",
+    left: "34%",
+    textStyle: {
+      color: "rgba(255, 255, 255, 1)"
+    }
+  },
+  xAxis: [
+    {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      axisPointer: {
+        type: 'shadow'
+      }
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value',
+      name: 'QTY',
+      min: 0,
+      max: 30,
+      interval: 10,
+      axisLabel: {
+        formatter: '{value}'
+      }
+    },
+    {
+      type: 'value',
+      name: 'Yield',
+      min: 85,
+      max: 100,
+      interval: 5,
+      axisLabel: {
+        formatter: '{value} %'
+      }
+    }
+  ],
+  series: [
+    {
+      name: 'Fail QTY',
+      type: 'bar',
+      tooltip: {
+        valueFormatter: function (value) {
+          return value;
+        }
+      },
+      itemStyle: {
+      color: "rgba(255, 17, 0, 1)"
+      },
+      data: [
+        2, 5, 9, 26, 7, 17, 15.6, 12, 8, 8, 6, 3
+      ]
+    },
+    {
+      name: 'Yield',
+      type: 'line',
+      yAxisIndex: 1,
+      lineStyle: {
+      color: "rgba(94, 255, 0, 1)"
+      },
+      tooltip: {
+        valueFormatter: function (value) {
+          return value + ' %';
+        }
+      },
+      data: [92.7, 92.2, 93.3, 94.5, 96.3, 90.2, 92.3, 93.4, 93.0, 96.5, 92.0, 96.2]
+    }
+  ]
+};
 function setCookie(cname,cvalue,exs){
     var d = new Date();
     d.setTime(d.getTime()+(exs*60*1000));
@@ -44,17 +316,15 @@ function update_time(){
 function on_click(){
     var username = getCookie("username");
     var pwd = getCookie("pwd");
-    alert(username,pwd);
     $.ajax({
             url:"/login",
             type:"post",
             data:{"username":username,"pwd":pwd},
             success:function(datas){
                 if (datas=="ok"){
-                    var url = window.location.href.split('/');
-                    var url1 = url[0]+"//"+url[1]+url[2];
-                    alert(url1);
-                    window.location.href=`${url1}/main?username=${username}&pwd=${pwd}`;
+                  var url = window.location.href.split('/');
+                  var url1 = url[0]+"//"+url[1]+url[2];
+                  window.location.href=`${url1}/main?username=${username}&pwd=${pwd}`;
                 }
                 else{
                     alert("密码错误！");
@@ -65,8 +335,25 @@ function on_click(){
         })
 }
 
+function get_cap_summary_data(){
+    $.ajax({    
+      url:"/capSummary",
+      timeout:10000,
+      success:function(datas){
+        cap_option.series[0].data=datas["qty"];
+        cap_option.series[1].data=datas["yield"];
+        capChart.setOption(cap_option);
+      },
+      error:function(xhr,type,errorThrown){
 
+      }
+  })
+}
 
+hsgChart.setOption(hsg_option);
+capChart.setOption(cap_option);
+idoChart.setOption(ido_option);
 clear_data()
 update_time()
 setInterval(update_time,1000)
+setInterval(get_cap_summary_data,2000)
